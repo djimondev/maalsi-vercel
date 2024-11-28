ac299baf1fd2dd8b35fe7fb40bdcc920b4460ec3
Jimmy LEFEVRE <2419238+djimondev@users.noreply.github.com>
2024-11-28 14:51:18 +0100

    ♻️ (VideoCard.jsx): Remove redundant role attribute from button element for cleaner code and to adhere to best practices.

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,7 +5,7 @@ export const VideoCard = ({ video, onClick }) => {
     const { id, title, thumbnail, channel, views, timestamp } = video;
 
     return (
-        <button className="flex flex-col cursor-pointer group" onClick={onClick} role="button">
+        <button className="flex flex-col cursor-pointer group" onClick={onClick}>
             <div className="relative aspect-video rounded-xl overflow-hidden bg-gray-400">
                 {thumbnail !== "" ? (
                     <img src={thumbnail} alt={title} className="object-cover w-full h-full group-hover:scale-105 transition-transform duration-200" />
