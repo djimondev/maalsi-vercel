fc1f29e36ba04eb1f37847a5e91a6473ad0490e3
Jimmy LEFEVRE <2419238+djimondev@users.noreply.github.com>
2024-11-28 14:47:26 +0100

    🐛 (VideoCard.jsx): Replace single quotes with double quotes in PropTypes import for code consistency ✨ (VideoCard.jsx): Add role="button" to div to improve accessibility 🐛 (VideoCard.jsx): Add newline at end of file to follow best practices

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,11 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import React from "react";
 
 export const VideoCard = ({ video, onClick }) => {
     const { id, title, thumbnail, channel, views, timestamp } = video;
 
     return (
-        <div className="flex flex-col cursor-pointer group" onClick={onClick}>
+        <div className="flex flex-col cursor-pointer group" onClick={onClick} role="button">
             <div className="relative aspect-video rounded-xl overflow-hidden bg-gray-400">
                 {thumbnail !== "" ? (
                     <img src={thumbnail} alt={title} className="object-cover w-full h-full group-hover:scale-105 transition-transform duration-200" />
